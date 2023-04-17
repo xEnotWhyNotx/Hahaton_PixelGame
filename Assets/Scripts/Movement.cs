f4bac5f3cdbc16cc142e75fb319ae657bf4078ca
Sergey Novichkov <NovichkovSD@yandex.ru>
2023-04-17 11:50:48 +0300

    Initial buildtest commit

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float speed;
-
+    public Animator animator;
     private Vector3 direction;
 
     private void Update()
@@ -14,9 +14,27 @@ public class Movement : MonoBehaviour
         float vertical = Input.GetAxisRaw("Vertical");
 
         direction = new Vector3(horizontal, vertical);
+        AnimateMovement(direction);
     }
     private void FixedUpdate()
     {
         this.transform.position += direction * speed * Time.deltaTime;
+    }
+
+    void AnimateMovement(Vector3 direction)
+    {
+        if (animator != null)
+        {
+            if(direction.magnitude > 0)
+            {
+                animator.SetBool("isMoving", true);
+                animator.SetFloat("horizontal", direction.x);
+                animator.SetFloat("vertical", direction.y);
+            }
+            else
+            {
+                animator.SetBool("isMoving", false);
+            }
+        }
     }
 }
