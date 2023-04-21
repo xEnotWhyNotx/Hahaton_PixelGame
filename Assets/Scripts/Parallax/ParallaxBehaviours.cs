0a5ac57f579b7c831e30aaed07471f812451e689
Sergey Novichkov <NovichkovSD@yandex.ru>
2023-04-21 23:15:44 +0300

    PreBuild update (Full map with colliders and parallax effect on OBS Scene)

---
+++ Assets/Scripts/Parallax/ParallaxBehaviours.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxBehaviours : MonoBehaviour
+{
+    [SerializeField] Transform followingTarget;
+    [SerializeField, Range(0f, 1f)] float parallaxStrength = 0.1f;
+    [SerializeField] bool disableVerticalParallax;
+    Vector3 targetPreviousPosition;
+
+    void Start()
+    {
+        if (!followingTarget)
+            followingTarget = Camera.main.transform;
+
+        targetPreviousPosition = followingTarget.position;
+    }
+
+    void Update()
+    {
+        var delta = followingTarget.position - targetPreviousPosition;
+
+        if (disableVerticalParallax)
+            delta.y = 0;
+
+        targetPreviousPosition = followingTarget.position;
+
+        transform.position += delta * parallaxStrength;
+    }
+}
